2ac3455c6397267f0d5b0ea164649190809eacc1
Vitaliy Borodin <kiverak@gmail.com>
2025-12-22 20:16:14 +0300

    add parser service into cloud properties

--- dreader-news-parser/src/main/java/dreadernewsparser/DreaderNewsParserApplication.java
+++ dreader-news-parser/src/main/java/dreadernewsparser/DreaderNewsParserApplication.java
@@ -3,9 +3,11 @@ package dreadernewsparser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class DreaderNewsParserApplication {
 
     static void main(String[] args) {
