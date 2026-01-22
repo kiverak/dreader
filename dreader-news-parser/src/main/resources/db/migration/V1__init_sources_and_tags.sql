18f769276acd7b0d763c4b9d7eb89e66f5e71491
Vitaliy Borodin <kiverak@gmail.com>
2026-01-22 16:23:20 +0300

    add Flyway to news parser service

--- dreader-news-parser/src/main/resources/db/migration/V1__init_sources_and_tags.sql
+++ dreader-news-parser/src/main/resources/db/migration/V1__init_sources_and_tags.sql
@@ -15,32 +15,6 @@ CREATE TABLE IF NOT EXISTS source
 
 CREATE INDEX IF NOT EXISTS idx_source_name_hash ON source USING hash (name);
 
-CREATE TABLE IF NOT EXISTS article
-(
-    id               BIGSERIAL PRIMARY KEY,
-    title            VARCHAR(255) NOT NULL,
-    views_count      INT,
-    comments_count   INT,
-    content          TEXT         NOT NULL,
-    short_content    TEXT         NOT NULL,
-    url              VARCHAR(255) NOT NULL UNIQUE,
-    image_url        VARCHAR(255),
-    source_id        BIGINT REFERENCES source (id),
-    publication_date TIMESTAMP,
-    pushed           BOOLEAN DEFAULT FALSE,
-    created_at       TIMESTAMP    NOT NULL,
-    updated_at       TIMESTAMP
-);
-
-CREATE INDEX IF NOT EXISTS idx_article_url_hash ON article USING hash (url);
-
-CREATE TABLE IF NOT EXISTS article_tag
-(
-    article_id BIGINT NOT NULL REFERENCES article (id),
-    tag_id     BIGINT NOT NULL REFERENCES tag (id),
-    PRIMARY KEY (article_id, tag_id)
-);
-
 CREATE TABLE IF NOT EXISTS source_default_tags
 (
     source_id BIGINT NOT NULL REFERENCES source (id),
@@ -59,7 +33,7 @@ ON CONFLICT (name) DO NOTHING;
 INSERT INTO source_default_tags (source_id, tag_id)
 SELECT s.id, t.id
 FROM source s
-JOIN tag t ON t.name IN ('ai', 'it', 'coding', 'programming', 'technology')
+         JOIN tag t ON t.name IN ('ai', 'it', 'coding', 'programming', 'technology')
 WHERE s.name = 'Habr News'
 ON CONFLICT DO NOTHING;
 
