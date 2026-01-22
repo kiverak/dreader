ccf500dc227c2c8209cb30377236a97f506031ec
Vitaliy Borodin <kiverak@gmail.com>
2026-01-22 16:22:57 +0300

    refactor after adding Kafka

--- dreader-news-parser/src/main/java/dreadernewsparser/service/TagService.java
+++ dreader-news-parser/src/main/java/dreadernewsparser/service/TagService.java
@@ -6,10 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class TagService {
@@ -24,29 +20,6 @@ public class TagService {
                     newTag.setName(name);
                     return tagRepository.save(newTag);
                 });
-    }
-
-    @Transactional
-    public List<Tag> getOrCreateByNames(List<String> names) {
-        List<Tag> existingTags = tagRepository.findAllByNameIn(names);
-        Set<String> existingNames = existingTags.stream()
-                .map(Tag::getName)
-                .collect(Collectors.toSet());
-
-        List<Tag> newTags = names.stream()
-                .filter(name -> !existingNames.contains(name))
-                .distinct()
-                .map(name -> {
-                    Tag tag = new Tag();
-                    tag.setName(name);
-                    return tag;
-                })
-                .collect(Collectors.toList());
-
-        if (!newTags.isEmpty()) {
-            existingTags.addAll(tagRepository.saveAll(newTags));
-        }
-        return existingTags;
     }
 
     @Transactional
